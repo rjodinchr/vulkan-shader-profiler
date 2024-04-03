0fb754340769894a3bbc73fd19c90982156eb963
Romaric Jodin <rjodin@chromium.org>
2024-04-03 09:59:56 +0200

    add tests disabling counters

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -8,3 +8,9 @@ VKSP_RUNNER=${VKSP_RUNNER:-"vulkan-shader-profiler-runner"}
 
 ${VKSP_RUNNER} -i "${SCRIPT_DIR}/example.spvasm" -v
 ${VKSP_RUNNER} -i "${SCRIPT_DIR}/example-counter.spvasm" | grep "my_section"
+${VKSP_RUNNER} -i "${SCRIPT_DIR}/example-counter.spvasm" -c
+if ${VKSP_RUNNER} -i "${SCRIPT_DIR}/example-counter.spvasm" -c | grep "my_section"
+then
+    echo "'my_section' should not be found"
+    exit -1
+fi
